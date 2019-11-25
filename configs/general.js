b0655fc907e54efa9f9034e25989da89a0905eec
Ben Carroll <ben.carroll@student.tsc.nsw.edu.au>
2019-11-25 15:56:57 +1100

    Match current pi settings

--- configs/general.js
+++ configs/general.js
@@ -1,10 +1,10 @@
 /* jshint esversion:6 */
 
 // gpio pins of buttons, minimum 2 buttons
-button_pin_arr = [18, 17, 27, 22]; // RPi.GPIO.BCM based
+button_pin_arr = [18, 25, 27, 22]; // RPi.GPIO.BCM based
 pressed_state = 1;
 buttons = [];
-reverse_keypad = false; // reverse mapping of buttons (0123 -> 3210)
+reverse_keypad = true; // reverse mapping of buttons (0123 -> 3210)
 
 // if enabled menus will only be accessible after `lock_code`
 // has been entered.
@@ -18,7 +18,7 @@ red_led_pin = 23;
 
 // pretty self explanatory
 loggingConfig = {
-  callTracingEnabled: false,
+  callTracingEnabled: true,
   infoEnabled: true,
-  serviceDebug: true
+  serviceDebug: false
 }; //errors always on as only triggered when termination required
